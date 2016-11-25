90e5e9b85990c89f940ef17b2dd67440527685a8
Evgenus <chernyshov.eugene@gmail.com>
2016-11-25 19:11:28 +0200

    important views

--- wikiapi/__init__.py
+++ wikiapi/__init__.py
@@ -4,6 +4,4 @@ __version__ = '0.0.1'
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from wikiapi.views import *
---
+++ wikiapi/views.py
@@ -0,0 +1,25 @@
+from wikiapi import app
+
+@app.route("/pages", methods=["POST"])
+def add_page(): 
+    raise NotImplementedError()
+
+@app.route("/pages", methods=["GET"])
+def list_pages(): 
+    raise NotImplementedError()
+
+@app.route('/pages/<string:page>', methods=['GET'])
+def get_page():
+    raise NotImplementedError()
+
+@app.route('/pages/<string:page>/versions', methods=['GET'])
+def get_page_versions():
+    raise NotImplementedError()
+
+@app.route('/pages/<string:page>', methods=['PUT'])
+def edit_page():
+    raise NotImplementedError()
+
+@app.route('/pages/<string:page>/versions', methods=['PUT'])
+def set_page_version():
+    raise NotImplementedError()
